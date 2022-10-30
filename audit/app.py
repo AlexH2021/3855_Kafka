ac48b7988eb4afecb114132a50360ef81403254b
A Ut Hong <33076940+AlexH2021@users.noreply.github.com>
2022-10-30 00:29:53 -0700

    Update app.py

--- audit/app.py
+++ audit/app.py
@@ -30,8 +30,24 @@ def get_account_reading(index):
   return { "message": "Not Found"}, 404
 
 def get_trade_reading(index):
+  hostname = "%s:%d" % (cfg.events["hostname"], cfg.events["port"])
+  client = KafkaClient(hosts=hostname)
+  topic = client.topics[str.encode(cfg.events["topic"])]
+  consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+  logger.info("Retrieving Trade at index %d" % index)
 
-  pass
+  try:
+    for msg in consumer:
+      msg_str = msg.value.decode('utf-8')
+      msg = json.loads(msg_str)
+      # if msg["accountID"] == index:
+      return msg, 200
+  except:
+    logger.error("No more messages found")
+
+  logger.error("Could not find Account at index %d" % index)
+  
+  return { "message": "Not Found"}, 404
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api('audit_api.yaml',strict_validation=True,validate_responses=True)
